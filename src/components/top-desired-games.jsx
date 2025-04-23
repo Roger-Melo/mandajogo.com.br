8c63da77e95c6e459693c93c6b5ef356955b1470
Roger-Melo <rogerw592@gmail.com>
2025-04-23 10:30:51 -0300

    wip: styling top desired games section

--- src/components/top-desired-games.jsx
+++ src/components/top-desired-games.jsx
@@ -1,17 +1,16 @@
+import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
-import { UserRoundPlus } from "lucide-react"
-
-const siteName = "MandaJogo"
 
 export function TopDesiredGames () {
   return (
     <div className="px-4 py-8 bg-slate-950 lg:py-16">
-      <h2>Games mais desejados</h2>
+      <h2 className="font-bold uppercase text-primary-blue mb-6">Os mais desejados</h2>
       <ul>
-        <li>
-          <img src="" alt="" />
-          <h3>Title</h3>
+        <li className="border-2 rounded-2xl border-white/20 p-4">
+          <img src="/covers/ps5/astro-bot.jpg" alt="Astro Bot" />
+          <h3 className="text-3xl uppercase font-semibold mt-4">Astro Bot</h3>
           <ul>
+            <Badge>PS5</Badge>
             <li>Tag 1</li>
             <li>Tag 2</li>
             <li>Tag 3</li>

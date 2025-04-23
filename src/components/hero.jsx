eaf1d1bb5ea710182ee2dfbe9e9459856e695852
Roger-Melo <rogerw592@gmail.com>
2025-04-22 21:56:52 -0300

    better indentation at hero markup

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -9,11 +9,18 @@ export function Hero () {
       <h1 className="text-3xl font-bold sm:text-4xl lg:text-5xl lg:max-w-2xl lg:mx-auto">
         Game usado juntando poeira na estante <span className="text-primary-yellow">nunca mais</span>!
       </h1>
-      <p className="text-lg mt-4 md:mx-auto md:max-w-3xl lg:text-xl lg:max-w-xl">Sabe aquele game que você já terminou e dificilmente voltará a jogar um dia? No <strong>{siteName}</strong> você encontra pessoas que podem estar interessadas nele!</p>
+
+      <p className="text-lg mt-4 md:mx-auto md:max-w-3xl lg:text-xl lg:max-w-xl">
+        Sabe aquele game que você já terminou e dificilmente voltará a jogar um dia? No <strong>{siteName}</strong> você encontra pessoas que podem estar interessadas nele!
+      </p>
+
       <Button className="my-4 py-5 w-3xs bg-primary-yellow text-primary-black hover:cursor-pointer hover:bg-primary-yellow/90 lg:my-8 lg:py-6 lg:w-80">
         <UserRoundPlus />Participar do {siteName}
       </Button>
-      <p className="text-sm md:w-lg md:mx-auto lg:max-w-2xl">O {siteName} é uma plataforma online especializada na troca de mídias físicas de jogos de videogame do Brasil, no ar desde 2025.</p>
+
+      <p className="text-sm md:w-lg md:mx-auto lg:max-w-2xl">
+        O {siteName} é uma plataforma online especializada na troca de mídias físicas de jogos de videogame do Brasil, no ar desde 2025.
+      </p>
     </div>
   )
 }

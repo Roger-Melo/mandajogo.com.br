a4ceb8da355d9e56f1522e854533119901f285a1
Roger-Melo <rogerw592@gmail.com>
2025-04-24 09:44:49 -0300

    fix flex column/row at TopDesiredGames component

--- src/components/top-desired-games.jsx
+++ src/components/top-desired-games.jsx
@@ -69,7 +69,7 @@ export function TopDesiredGames () {
   return (
     <section className="px-4 py-8 bg-slate-950 lg:py-16 md:max-w-[1120px] md:mx-auto">
       <h2 className="font-bold uppercase text-primary-blue mb-6">Os mais desejados</h2>
-      <ul className="sm:flex sm:flex-wrap sm:gap-4">
+      <ul className="flex flex-col gap-4 sm:flex-row sm:flex-wrap sm:gap-4">
         {games.map((game) => (
           <GameCard
             key={game.title}

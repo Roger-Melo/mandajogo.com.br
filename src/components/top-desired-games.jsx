4d1ff129f4ac6afa1ed6bb2f2e052729a78b6b26
Roger-Melo <rogerw592@gmail.com>
2025-04-24 09:41:26 -0300

    finish TopDesiredGames responsive styling

--- src/components/top-desired-games.jsx
+++ src/components/top-desired-games.jsx
@@ -7,9 +7,14 @@ import Image from "next/image"
 
 const games = [
   { title: "Astro Bot", platform: "PS5", desired: 70, offered: 30, rating: 9.3, cover: "/covers/ps5/astro-bot.jpg" },
-  { title: "Suikoden I & II HD Remaster Gate Rune and Dunan Unification Wars", platform: "PS5", desired: 249, offered: 134, rating: 9.2, cover: "/covers/ps5/spider-man-2.jpg" },
-  // { title: "Spider-Man 2", platform: "PS5", desired: 249, offered: 134, rating: 9.2, cover: "/covers/ps5/spider-man-2.jpg" },
+  { title: "Spider-Man 2", platform: "PS5", desired: 249, offered: 134, rating: 9.2, cover: "/covers/ps5/spider-man-2.jpg" },
   { title: "Stellar Blade", platform: "PS5", desired: 73, offered: 31, rating: 8.8, cover: "/covers/ps5/stellar-blade.jpg" },
+  { title: "Astro Bot 2", platform: "PS5", desired: 70, offered: 30, rating: 9.3, cover: "/covers/ps5/astro-bot.jpg" },
+  { title: "Spider-Man 3", platform: "PS5", desired: 249, offered: 134, rating: 9.2, cover: "/covers/ps5/spider-man-2.jpg" },
+  { title: "Stellar Blade 2", platform: "PS5", desired: 73, offered: 31, rating: 8.8, cover: "/covers/ps5/stellar-blade.jpg" },
+  { title: "Astro Bot 3", platform: "PS5", desired: 70, offered: 30, rating: 9.3, cover: "/covers/ps5/astro-bot.jpg" },
+  { title: "Spider-Man 4", platform: "PS5", desired: 249, offered: 134, rating: 9.2, cover: "/covers/ps5/spider-man-2.jpg" },
+  { title: "Stellar Blade 3", platform: "PS5", desired: 73, offered: 31, rating: 8.8, cover: "/covers/ps5/stellar-blade.jpg" },
 ]
 
 function GameBadge ({ text }) {
@@ -20,20 +25,20 @@ function GameCard ({ title, platform, desired, offered, rating, cover }) {
   const badgeTexts = [platform, `${desired} desejos`, `${offered} ofertas`, `Nota ${rating}`]
   const gamePageLink = "#"
   return (
-    <li className="border-2 rounded-2xl border-white/20 p-4 ssm:flex ssm:columns-2 ssm:gap-4">
+    <li className="w-full gap-4 sm:w-[calc(50%-0.5rem)] border-2 rounded-2xl border-white/20 p-4 flex flex-col lg:w-[calc(33%-0.5rem)]">
       {/* image */}
-      <Link href={gamePageLink} className="ssm:flex-shrink-0 ssm:w-[183px]">
+      <Link href={gamePageLink} className="ssm:flex-shrink-0 ssm:w-[183px] sm:mx-auto">
         <Image width={236} height={294} src={cover} alt={title} className="mx-auto w-full h-auto" />
       </Link>
 
-      <div>
+      <div className="ssm:flex ssm:flex-col ssm:justify-between ssm:w-full sm:h-full">
         {/* title */}
         <Link href={gamePageLink}>
-          <h3 className="wrap-anywhere text-3xl text-center uppercase font-semibold my-4 ssm:text-left ssm:mt-0">{title}</h3>
+          <h3 className="wrap-anywhere text-3xl text-center uppercase font-semibold my-4 ssm:text-left ssm:mt-0 sm:text-center">{title}</h3>
         </Link>
 
         {/* bottom container */}
-        <div className="flex justify-between items-center gap-4">
+        <div className="flex justify-between items-center gap-4 ssm:items-end">
           {/* badges list */}
           <ul>
             {badgeTexts.map((text) => <GameBadge key={text} text={text} />)}
@@ -62,9 +67,9 @@ function GameCard ({ title, platform, desired, offered, rating, cover }) {
 
 export function TopDesiredGames () {
   return (
-    <section className="px-4 py-8 bg-slate-950 lg:py-16">
+    <section className="px-4 py-8 bg-slate-950 lg:py-16 md:max-w-[1120px] md:mx-auto">
       <h2 className="font-bold uppercase text-primary-blue mb-6">Os mais desejados</h2>
-      <ul className="flex flex-col gap-4">
+      <ul className="sm:flex sm:flex-wrap sm:gap-4">
         {games.map((game) => (
           <GameCard
             key={game.title}

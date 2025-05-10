bd597ed6cee29c2167ff2d58033e421a417022b0
Roger-Melo <rogerw592@gmail.com>
2025-05-10 16:23:24 -0300

    add links dinamically

--- src/components/menu-bar.jsx
+++ src/components/menu-bar.jsx
@@ -2,28 +2,27 @@ import Link from "next/link"
 import { Handshake, MessageCircle, Heart, Library, MenuIcon } from "lucide-react"
 import { tempLink } from "@/lib/utils"
 
+const links = [
+  { href: tempLink, text: "Oportunidades", icon: Handshake },
+  { href: tempLink, text: "Mensagens", icon: MessageCircle },
+  { href: tempLink, text: "Desejos", icon: Heart },
+  { href: tempLink, text: "Coleção", icon: Library },
+]
+
+function MenuBarLink ({ href, text, icon: Icon }) {
+  return (
+    <Link href={href} className="flex flex-col items-center justify-center gap-1">
+      <Icon className="h-5 w-5" />
+      <span className="text-xs">{text}</span>
+    </Link>
+  )
+}
+
 export function MenuBar () {
   return (
     <nav className="fixed bottom-0 left-0 right-0 z-50 flex items-center justify-between bg-primary-blue text-white w-full px-2 py-3">
-      <Link href={tempLink} className="flex flex-col items-center justify-center">
-        <Handshake className="h-5 w-5 mb-1" />
-        <span className="text-xs">Oportunidades</span>
-      </Link>
-
-      <div className="flex flex-col items-center justify-center">
-        <MessageCircle className="h-5 w-5 mb-1" />
-        <span className="text-xs">Mensagens</span>
-      </div>
-
-      <div className="flex flex-col items-center justify-center">
-        <Heart className="h-5 w-5 mb-1" />
-        <span className="text-xs">Desejos</span>
-      </div>
-
-      <div className="flex flex-col items-center justify-center">
-        <Library className="h-5 w-5 mb-1" />
-        <span className="text-xs">Coleção</span>
-      </div>
+      {links.map((link) =>
+        <MenuBarLink key={link.text} href={link.href} text={link.text} icon={link.icon} />)}
 
       <div className="flex flex-col items-center justify-center">
         <MenuIcon className="h-5 w-5 mb-1" />

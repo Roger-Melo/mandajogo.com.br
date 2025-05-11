3cb6345f34db8114be3cdedbb839b163762cb891
Roger-Melo <rogerw592@gmail.com>
2025-05-11 12:53:43 -0300

    wip: reestructuring MenuBar markup

--- src/components/menu-bar.jsx
+++ src/components/menu-bar.jsx
@@ -1,9 +1,8 @@
 import Link from "next/link"
 import { Handshake, MessageCircle, Heart, Library, MenuIcon } from "lucide-react"
 import { tempLink } from "@/lib/utils"
-import { Fragment } from "react"
 
-const links = [
+const mobileLinks = [
   { href: tempLink, text: "Oportunidades", icon: Handshake },
   { href: tempLink, text: "Mensagens", icon: MessageCircle },
   { href: tempLink, text: "Desejos", icon: Heart },
@@ -21,8 +20,23 @@ function MenuBarLink ({ href, text, icon: Icon }) {
 
 export function MenuBar () {
   return (
+    <nav>
+      <div>
+        <strong>MandaJogo - Comunidade de Troca de Jogos de Videogame</strong>
+        <ul>
+          <li>Oportunidades</li>
+          <li>Mensagens</li>
+          <li>Desejos</li>
+          <li>Coleção</li>
+        </ul>
+        <button>Menu</button>
+      </div>
+    </nav>
+  )
+  // 👇🏻 not commented for now to keep the syntax highlight & renaming identifier benefits
+  return (
     <nav className="fixed bottom-0 left-0 right-0 z-50 flex items-center justify-between bg-primary-blue text-white w-full px-2 py-3 ssm:px-4 md:bottom-auto md:top-0 md:bg-primary-black md:px-10">
-      {links.map((link) => <MenuBarLink key={link.text} href={link.href} text={link.text} icon={link.icon} />)}
+      {mobileLinks.map((link) => <MenuBarLink key={link.text} href={link.href} text={link.text} icon={link.icon} />)}
 
       {/* Mobile-only Menu button */}
       <div className="flex flex-col items-center justify-center md:hidden">

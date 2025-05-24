75d906caa31ffcd22aa730f4754af35acddbc072
Roger-Melo <rogerw592@gmail.com>
2025-05-24 10:42:51 -0300

    menu-bar: [refac] create getLinks function

--- src/components/menu-bar.jsx
+++ src/components/menu-bar.jsx
@@ -5,28 +5,6 @@ import Link from "next/link"
 import { LogIn, LogOut, Settings, ArrowRightLeft, ListChecks, Handshake, MessageCircle, Heart, Library, MenuIcon, UserRoundPlus } from "lucide-react"
 import { cn, tempLink } from "@/lib/utils"
 
-const oportunitiesLink = { id: Math.random(), href: tempLink, text: "Oportunidades", icon: Handshake }
-const messagesLink = { id: Math.random(), href: tempLink, text: "Mensagens", icon: MessageCircle }
-const desiresLink = { id: Math.random(), href: tempLink, text: "Desejos", icon: Heart }
-const collectionLink = { id: Math.random(), href: tempLink, text: "Coleção", icon: Library }
-
-const loggedUserMobileLinks = [oportunitiesLink, messagesLink, desiresLink, collectionLink]
-
-const loggedUserMdLinks = [
-  oportunitiesLink,
-  { id: Math.random(), href: tempLink, text: "Propostas", icon: ListChecks },
-  { id: Math.random(), href: tempLink, text: "Trocas", icon: ArrowRightLeft },
-  desiresLink,
-  collectionLink,
-  messagesLink,
-  { id: Math.random(), href: tempLink, text: "Preferências", icon: Settings },
-]
-
-const notLoggedUserLinks = [
-  { id: Math.random(), href: tempLink, text: "Participar do MandaJogo", icon: UserRoundPlus },
-  { id: Math.random(), href: tempLink, text: "Acessar minha conta", icon: LogIn },
-]
-
 function MenuBarLink ({ href, text, icon: Icon, user }) {
   return (
     <Link href={href} className={cn("flex flex-col items-center justify-center gap-1", user ? "md:flex-row" : "ssm:flex-row")}>
@@ -52,12 +30,42 @@ function useBreakpoint () {
   return { isMdBreakpoint }
 }
 
-export function MenuBar () {
-  const { isMdBreakpoint } = useBreakpoint()
+function getLinks ({ user, isMdBreakpoint }) {
+  if (!user) {
+    const notLoggedUserLinks = [
+      { id: Math.random(), href: tempLink, text: "Participar do MandaJogo", icon: UserRoundPlus },
+      { id: Math.random(), href: tempLink, text: "Acessar minha conta", icon: LogIn },
+    ]
+    return notLoggedUserLinks
+  }
+
+  const oportunitiesLink = { id: Math.random(), href: tempLink, text: "Oportunidades", icon: Handshake }
+  const messagesLink = { id: Math.random(), href: tempLink, text: "Mensagens", icon: MessageCircle }
+  const desiresLink = { id: Math.random(), href: tempLink, text: "Desejos", icon: Heart }
+  const collectionLink = { id: Math.random(), href: tempLink, text: "Coleção", icon: Library }
+
+  const loggedUserMobileLinks = [oportunitiesLink, messagesLink, desiresLink, collectionLink]
+
+  const loggedUserMdLinks = [
+    oportunitiesLink,
+    { id: Math.random(), href: tempLink, text: "Propostas", icon: ListChecks },
+    { id: Math.random(), href: tempLink, text: "Trocas", icon: ArrowRightLeft },
+    desiresLink,
+    collectionLink,
+    messagesLink,
+    { id: Math.random(), href: tempLink, text: "Preferências", icon: Settings },
+  ]
+
   const loggedUserLinks = isMdBreakpoint
     ? loggedUserMdLinks.map((link) => ["Mensagens", "Preferências"].some((t) => t === link.text) ? { ...link, text: "" } : link)
     : loggedUserMobileLinks
-  const user = false // this const is a temp placeholder to test markup & styling
+  return loggedUserLinks
+}
+
+export function MenuBar () {
+  const { isMdBreakpoint } = useBreakpoint()
+  const user = true // this const is a temp placeholder to test markup & styling
+  const links = getLinks({ user, isMdBreakpoint })
   return (
     <nav className="bg-primary-blue text-white fixed bottom-0 left-0 right-0 z-50 md:bg-slate-950 md:bottom-auto md:top-0">
       <div className="flex items-center justify-between px-2 py-3 gap-4 md:px-10 lg:max-w-site-width lg:mx-auto xl:px-0">
@@ -65,8 +73,7 @@ export function MenuBar () {
         <strong className="hidden xl:block xl:text-xs xl:uppercase">MandaJogo - Comunidade de Troca de Jogos de Videogame</strong>
 
         <ul className="flex justify-around flex-1 md:items-center md:justify-end md:gap-3">
-          {!user && notLoggedUserLinks.map((link) => <MenuBarLink key={link.id} href={link.href} text={link.text} icon={link.icon} user={user} />)}
-          {user && loggedUserLinks.map((link) => <MenuBarLink key={link.id} href={link.href} text={link.text} icon={link.icon} user={user} />)}
+          {links.map((link) => <MenuBarLink key={link.id} href={link.href} text={link.text} icon={link.icon} user={user} />)}
           {/* mobile-and-logged-user-only Menu button */}
           {user && (
             <li className="flex flex-col items-center justify-center md:hidden">

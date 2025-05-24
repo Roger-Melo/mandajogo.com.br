495d579fe7a14ce259957b4f4ef06fb6b39cdddc
Roger-Melo <rogerw592@gmail.com>
2025-05-24 14:41:34 -0300

    create Logo component

---
+++ src/components/logo.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export function Logo () {
+  return (
+    <Link href="/">
+      <Image
+        priority
+        width={484}
+        height={128}
+        sizes="(min-width: 1420px) 484px, (min-width: 1220px) calc(90vw - 776px), (min-width: 1040px) calc(15vw + 122px), (min-width: 640px) calc(15.53vw + 142px), (min-width: 560px) 484px, 90vw"
+        src="/logo-manda-jogo.png"
+        alt="Logo MandaJogo"
+        className="w-32 h-auto sm:w-37 md:w-52 lg:w-64"
+      />
+    </Link>
+  )
+}

e6d6b0d4e22af6b1f5c9ab838987eb201e69c83a
Roger-Melo <rogerw592@gmail.com>
2025-05-26 13:38:09 -0300

    using newReleases array

--- src/components/game-card.jsx
+++ src/components/game-card.jsx
@@ -8,7 +8,7 @@ import { PlatformsList } from "@/components/platforms-list"
 import { ReusableTooltip } from "@/components/reusable-tooltip"
 
 function GameBadgesList ({ game }) {
-  const badgeTexts = [game.platform, `${game.desired} desejos`, `${game.offered} ofertas`, `Nota ${game.rating}`]
+  const badgeTexts = [game.platform.name, `${game.wishes} desejos`, `${game.offers} ofertas`, `Nota ${game.rating}`]
   return (
     <ul>
       {badgeTexts.map((text, index) =>
@@ -26,11 +26,11 @@ function GameDescription ({ game }) {
   )
 }
 
-function GamePageButton ({ cardType }) {
+function GamePageButton ({ cardType, gamePageLink }) {
   return (
     <ReusableTooltip text="Ver jogo">
-      <Button asChild size="icon" className={cn(`bg-transparent border-white/20 border-2 hover:bg-transparent hover:cursor-pointer hover:border-white/10`, cardType === "newReleaseGame" ? "self-end border-primary-blue hover:border-primary-blue" : "")}>
-        <Link href="#">
+      <Button asChild size="icon" className={cn("bg-transparent border-white/20 border-2 hover:bg-transparent hover:cursor-pointer hover:border-white/10", cardType === "newReleaseGame" ? "self-end border-primary-blue hover:border-primary-blue" : "")}>
+        <Link href={gamePageLink}>
           <MoveRight className={cardType === "newReleaseGame" ? "text-primary-blue" : ""} />
         </Link>
       </Button>
@@ -39,13 +39,14 @@ function GamePageButton ({ cardType }) {
 }
 
 export function GameCard ({ cardType, game }) {
+  const gamePageLink = `/game/${game.platform.slug}/${game.slug}`
   return (
     <li className={cn("break-inside-avoid mb-4 w-full border-2 rounded-2xl border-white/20 p-4 flex flex-col gap-4", cardType === "newReleaseGame" ? "border-primary-blue h-fit" : "")}>
-      <Link href="#" className="ssm:flex-shrink-0 ssm:w-[183px] sm:mx-auto">
-        <Image width={236} height={294} src={game.cover} alt={game.title} className="mx-auto w-full h-auto" />
+      <Link href={gamePageLink} className="ssm:flex-shrink-0 ssm:w-[183px] sm:mx-auto">
+        <Image width={236} height={294} src={`/covers/${game.platform.slug}/${game.imageCover}`} alt={game.title} className="mx-auto w-full h-auto" />
       </Link>
       <div className={cn("ssm:flex ssm:flex-col ssm:w-full sm:h-full", cardType === "newReleaseGame" ? "" : "ssm:justify-between")}>
-        <Link href="#">
+        <Link href={gamePageLink}>
           <h3 className={cn("wrap-anywhere text-3xl text-center uppercase font-semibold my-4 ssm:text-left ssm:mt-0 sm:text-center", { "text-primary-blue": cardType === "newReleaseGame" })}>{game.title}</h3>
         </Link>
         <div className={cn("flex justify-between items-center gap-4 ssm:items-end", { "flex-col": cardType === "newReleaseGame" })}>
@@ -58,7 +59,7 @@ export function GameCard ({ cardType, game }) {
             )
             : <GameBadgesList game={game} />
           }
-          {cardType === "topGame" && <GamePageButton cardType={cardType} />}
+          {cardType === "topGame" && <GamePageButton cardType={cardType} gamePageLink={gamePageLink} />}
         </div>
       </div>
     </li>

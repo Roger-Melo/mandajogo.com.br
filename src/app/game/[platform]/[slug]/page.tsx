8e284edfee829dcbafccf9dba22ad01af1686239
Roger-Melo <rogerw592@gmail.com>
2025-06-02 09:13:15 -0300

    create src/types/index.ts file

--- src/app/game/[platform]/[slug]/page.tsx
+++ src/app/game/[platform]/[slug]/page.tsx
@@ -8,41 +8,10 @@ import { GameCard } from "@/components/game-card"
 import { topDesiredGames } from "@/db/sample-data/top-desired-games"
 import { gameOwners } from "@/db/sample-data/game-owners"
 import { getBoxCondition, getInterestLevels, getMediaCondition, getBookletCondition } from "@/lib/utils"
+import type { User } from "@/types"
 
 type GamePageProps = {
   params: Promise<{ slug: string; platform: string }>
-}
-
-// temp
-type User = {
-  name: string
-  username: string
-  imageAvatar: string
-  isVerified: boolean
-  supplyName: string | null
-  imageCover: string | null
-  supplyPermalink: string | null
-  bundlePermalink: string | null
-  scoreGeneral: number
-  exchangesCount: number
-  enumLevel: number
-  enumLevelDesc: string | null
-  conditionMedia: number
-  conditionMediaDesc: string | null
-  conditionBooklet: number
-  conditionBookletDesc: string | null
-  conditionBox: number
-  conditionBoxDesc: string | null
-  enumVersion: number
-  enumVersionDesc: string | null
-  enumRegion: number
-  enumRegionDesc: string | null
-  notes: string | null
-  city: string
-  state: string
-  distance: number
-  lat: number
-  lng: number
 }
 
 // temp
---
+++ src/types/index.ts
@@ -0,0 +1,31 @@
+// temp
+export type User = {
+  name: string
+  username: string
+  imageAvatar: string
+  isVerified: boolean
+  supplyName: string | null
+  imageCover: string | null
+  supplyPermalink: string | null
+  bundlePermalink: string | null
+  scoreGeneral: number
+  exchangesCount: number
+  enumLevel: number
+  enumLevelDesc: string | null
+  conditionMedia: number
+  conditionMediaDesc: string | null
+  conditionBooklet: number
+  conditionBookletDesc: string | null
+  conditionBox: number
+  conditionBoxDesc: string | null
+  enumVersion: number
+  enumVersionDesc: string | null
+  enumRegion: number
+  enumRegionDesc: string | null
+  notes: string | null
+  city: string
+  state: string
+  distance: number
+  lat: number
+  lng: number
+}

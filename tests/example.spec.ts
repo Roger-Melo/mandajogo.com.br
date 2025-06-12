9fe83a7ea560c70e32670e29d0f5fd7a8980286e
Roger-Melo <rogerw592@gmail.com>
2025-06-12 09:20:38 -0300

    e2e: add waitForUrl to navigation test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,9 +3,11 @@ import { test, expect, type Page } from "@playwright/test"
 test.describe("HomePage", () => {
   test("should navigate to the Elden Ring page", async ({ page }: { page: Page }) => {
     const gameTitle = "Elden Ring"
+    const gameLink = "/game/ps5/elden-ring"
     await page.goto("/")
     await page.click(`text=${gameTitle}`)
-    await expect(page).toHaveURL("/game/ps5/elden-ring")
+    await page.waitForURL(gameLink)
+    await expect(page).toHaveURL(gameLink)
     await expect(page.getByRole("heading", { name: gameTitle })).toBeVisible()
   })
 })

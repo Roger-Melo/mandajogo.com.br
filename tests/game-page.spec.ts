aef097fb589d9d0fabb1e4b64fe8cdba313615ee
Roger-Melo <rogerw592@gmail.com>
2025-06-13 09:41:19 -0300

    e2e: add the 'should display the initial game owner cards' test

--- tests/game-page.spec.ts
+++ tests/game-page.spec.ts
@@ -1,5 +1,16 @@
 import { test, expect, type Page } from "@playwright/test"
 import { ownersPerPage } from "@/lib/constants"
+import prisma from "@/lib/db"
+
+async function getGameOwners() {
+  const gameOwners = await prisma.gameOwner.findMany({ orderBy: { enumLevel: "desc" } })
+  const totalCount = await prisma.gameOwner.count()
+  return { gameOwners, totalCount }
+}
+
+function getGameOwnersList(page: Page) {
+  return page.getByTestId("game-owners-list")
+}
 
 test.describe("GamePage", () => {
   test.beforeEach(async ({ page }) => {
@@ -10,13 +21,23 @@ test.describe("GamePage", () => {
   })
 
   test("should display the game owners list", async ({ page }: { page: Page }) => {
-    const gameOwnersList = page.getByTestId("game-owners-list")
+    const gameOwnersList = getGameOwnersList(page)
     await gameOwnersList.isVisible()
   })
 
-  test("should display the right quantity of proposal buttons", async ({ page }: { page: Page }) => {
-    const proposalButtons = page
-      .getByTestId("game-owners-list").getByRole("button", { name: "Fazer proposta" })
+  test("should display the initial game owner cards", async ({ page }: { page: Page }) => {
+    const { gameOwners } = await getGameOwners()
+    const initialPageGameOwners = gameOwners.slice(0, ownersPerPage)
+    const gameOwnersList = getGameOwnersList(page)
+
+    for (const initialGameOwner of initialPageGameOwners) {
+      const ownerName = gameOwnersList.getByRole("heading", { name: initialGameOwner.name })
+      await expect(ownerName).toBeVisible()
+      const ownerImg = gameOwnersList.getByAltText(`Imagem ${initialGameOwner.name}`)
+      await expect(ownerImg).toBeVisible()
+    }
+
+    const proposalButtons = gameOwnersList.getByRole("button", { name: "Fazer proposta" })
     await expect(proposalButtons).toHaveCount(ownersPerPage)
   })
 })

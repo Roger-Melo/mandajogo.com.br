b23d52f66566a3b0ae155dd94d43910b639f06d5
Roger Melo <rogerw592@gmail.com>
2025-12-15 14:54:06 -0300

    refactor(components): convert PlatformsList to TypeScript
    The PlatformsList component has been converted from JavaScript (.jsx) to
    TypeScript (.tsx) to improve type safety and align with the project's
    overall migration to TypeScript.
    
    This refactoring introduces the 'PlatformsListProps' type, which explicitly
    defines the expected props for the component. This change enhances
    code readability and maintainability without altering the component's
    functionality.

--- src/components/platforms-list.tsx
+++ src/components/platforms-list.tsx
@@ -2,15 +2,20 @@ import Link from "next/link"
 import { BaseTooltip } from "@/components/base-tooltip"
 import { cn } from "@/lib/utils"
 
-export function PlatformsList ({ data, tooltipText, logosWidth, linksType }) {
-  // linksType: "platform" | "game"
+type PlatformsListProps = {
+  tooltipText: string
+  logosWidth: string
+  linksType: "platform" | "game"
+}
+
+export function PlatformsList ({ data, tooltipText, logosWidth, linksType }: PlatformsListProps) {
   const dataArray = linksType === "game" ? data.platforms : data
   return (
     <ul className={cn("flex gap-2", linksType === "game" ? "" : "flex-wrap justify-center px-4")}>
       {dataArray.map((item, index) =>
         <li key={index}>
           <BaseTooltip text={tooltipText}>
-            <Link href={linksType === "game" ? `/game/${item.slug}/${data.slug}` : `#`}>
+            <Link href={linksType === "game" ? `/game/${item.slug}/${data.slug}` : '#'}>
               <img className={cn("h-auto", logosWidth)} src={`/svg/logos/${item.slug}.svg`} alt={`${item.name} logo`} />
             </Link>
           </BaseTooltip>
